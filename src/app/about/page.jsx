77bddc9c941faf5c1b302b2592d5f5660b05c161
Assem Mohamed <129564950+Assem29@users.noreply.github.com>
2023-04-26 23:44:53 +0200

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "../../components/Header";
+
+export default function About() {
+  return (
+    <div>
+      <div className="mx-4 max-w-6xl sm:mx-auto py-8">
+        <h1 className="text-4xl font-bold mb-4">About Us</h1>
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel
+          tincidunt dolor. Sed euismod dui quis nibh consectetur, vel interdum
+          ante malesuada. Nulla facilisi. Integer efficitur ut sapien vel
+          vestibulum. Nunc id lectus fermentum, consequat metus ut, tempor
+          lorem.
+        </p>
+        <p className="mb-4">
+          Fusce eget interdum lectus. Sed euismod enim in turpis malesuada, non
+          elementum est lacinia. In eget lorem mauris. Morbi faucibus metus ac
+          lectus lacinia, non finibus purus feugiat. Suspendisse quis ultrices
+          turpis. Etiam laoreet ex at mauris vehicula lacinia.
+        </p>
+        <p className="mb-4">
+          Vestibulum sit amet tortor sed ligula ultricies porttitor eu in
+          dolor. Aenean facilisis ultrices odio, eget suscipit quam. Suspendisse
+          potenti. Vestibulum gravida turpis sed mi euismod, at fermentum elit
+          ullamcorper.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import MenuItems from "./MenuItems";
-import { FiHome } from "react-icons/fi";
-import { AiOutlineInfoCircle } from "react-icons/ai";
+import { FaHome } from "react-icons/fa";
+import { FaInfoCircle } from "react-icons/fa";
 import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
-        <MenuItems title="HOME" address="/" Icon={FiHome} />
-        <MenuItems title="ABOUT" address="/about" Icon={AiOutlineInfoCircle} />
+        <MenuItems title="HOME" address="/" Icon={FaHome} />
+        <MenuItems title="ABOUT" address="/about" Icon={FaInfoCircle} />
       </div>
       <div className="flex items-center space-x-5">
-        <DarkModeSwitch/>
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
