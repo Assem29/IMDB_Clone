7bb9bc60b6431ec3a6afe3c7db1aaf3d0daa888f
Assem Mohamed <129564950+Assem29@users.noreply.github.com>
2023-04-28 01:31:34 +0300

    fetch movie

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import NavbarItem from './NavbarItem'
 export default function Navbar() {
   return (
     <div className='flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-3'>
-      <NavbarItem title="Trending" param="FetchTrend"/>
-      <NavbarItem title="Top Rated" param="FetchData"/>
+       <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
 
     </div>
   )
